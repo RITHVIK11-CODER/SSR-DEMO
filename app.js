f2130942c60fd76b0330acde3e1e3e78649654a5
RITHVIK11-CODER <rithviknag1177@gmail.com>
2026-07-30 22:19:13 +0530

    chore: configure application for Render deployment

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const path = require("path");
-const routes = require("./routes/routes");
 
 const app = express();
 
-app.set("view engine","ejs");
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine", "ejs");
 
-app.use("/",routes);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000,()=>{
-    console.log("Server running on http://localhost:3000");
+const routes = require("./routes/routes");
+app.use("/", routes);
+
+// IMPORTANT: Use Render's PORT
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
